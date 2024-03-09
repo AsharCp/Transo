7ada3b816c7f2e702465aa8bc1ecb59a0a131f33
Your Name <you@example.com>
2024-03-09 11:57:45 +0530

    Integrated frontend and backend

--- Backend/app.py
+++ Backend/app.py
@@ -1,13 +1,25 @@
 # Install sependencies
 # !pip install langdetect
 # !pip install --upgrade googletrans==4.0.0-rc1
+# pip install flask
 
+from flask import Flask,request,jsonify
 from langdetect import detect_langs
 import googletrans
 from googletrans import Translator
-sentance = "Cómo estás"
+# sentance = "Cómo estás"
 
-# from googletrans import Translator
-translator = Translator()
-translated_language = translator.translate(sentance,dest='en')
-print(translated_language.text)
+app = Flask(__name__)
+@app.route('/convert', methods=['POST'])
+def translate_text():
+    data = request.json
+    text = data['input_text']
+    to_language = data['to_lang']  
+    translator = Translator()
+    translatedText = translator.translate(text,dest=to_language)
+    return jsonify({'translated_text': translatedText.text})
+
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
--- transo/src/App.js
+++ transo/src/App.js
@@ -5,32 +5,58 @@ import './App.css';
 function App() {
 
   const [text,setText] = useState("")
+  const [outText,setOutText] = useState("")
+  const [language,setLanguage] = useState("")
 
   const handleText=(event)=>{
     setText(event.target.value)
     console.log(text)
 
   }
+
+  const handleSelectChange=(event)=>{
+    setLanguage(event.target.value)
+
+  }
+
+
   const handleTranslate=()=>{
-    console.log("Clicked Yes")
+    fetch("/convert", {
+      method:'POST',
+      headers:{
+        'Content-Type':'application/json',
+      },
+      body: JSON.stringify({input_text:text,to_lang:language})
+    })
+    .then(response=>response.json())
+    .then(data=>{setOutText(data.translated_text)})
+    .catch(error=>console.log(error))
   }
 
   const handleClear=()=>{
     setText("");
+    setOutText("");
   }
+
 
   
 
   return (
     <div className="home">
-      <h1 className="heading">Lets's Communicate😉</h1>
+      <h1 className="heading">Let's Communicate😉</h1>
       <div className="textDiv">
-        <textarea value={text} className="textIn" type='text' placeholder="Enter text" onChange={handleText}></textarea>
+        <textarea value={text} className="textIn" type='text' placeholder="Enter text" onChange={handleText} spellCheck="false"></textarea>
         <div className='btnDiv'>
+          <select id='dropmenu' onChange={handleSelectChange}>
+            <option value="es">Spanish</option>
+            <option value="ml">Malayalam</option>
+            <option value="ta">Tamil</option>
+            <option value="ar">Arabic</option>
+          </select>
           <button className="translateBtn" onClick={handleTranslate} >Translate</button>
           <button className="clearBtn" onClick={handleClear}>Clear</button>
         </div>
-        <textarea className="textOut" type="text" placeholder="Translate"></textarea>
+        <textarea value={outText} className="textOut" type="text" placeholder="Translate" spellCheck="false"></textarea>
       </div>
      
     </div>
